cd3ea3d3b3b7a06820067d590ff3821dde881b1f
Kai Larson <kairblarson@gmail.com>
2023-01-22 11:00:40 -0600

    added middleware

---
+++ react_social/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://tiresome-bead-production.up.railway.app/',
+      changeOrigin: true,
+    })
+  );
+};
